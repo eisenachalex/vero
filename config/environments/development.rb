309ae4d7f18f08715f2421af6c79a9869919fc41
Alex Eisenach <hoopmiester@Alexs-MacBook-Pro-3.local>
2013-12-04 19:04:57 -0600

    reverted

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -35,15 +35,15 @@ Vero::Application.configure do
   # Expands the lines which load the assets
 
   # Don't care if the mailer can't send
-# config.action_mailer.delivery_method = :smtp
-# config.action_mailer.smtp_settings = {
-#   address:              'smtp.gmail.com',
-#   port:                 587,
-#   domain:               'gmail.com',
-#   user_name:            'customtechfront@gmail.com',
-#   password:             'albutler92',
-#   authentication:       'plain',
-#   enable_starttls_auto: true  }
+config.action_mailer.delivery_method = :smtp
+config.action_mailer.smtp_settings = {
+  address:              'smtp.gmail.com',
+  port:                 587,
+  domain:               'gmail.com',
+  user_name:            'customtechfront@gmail.com',
+  password:             'albutler92',
+  authentication:       'plain',
+  enable_starttls_auto: true  }
  
    config.assets.debug = true
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,15 +61,15 @@ Vero::Application.configure do
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
 
-#     config.action_mailer.delivery_method = :smtp
-# config.action_mailer.smtp_settings = {
-#   address:              'smtp.gmail.com',
-#   port:                 587,
-#   domain:               'gmail.com',
-#   user_name:            'customtechfront@gmail.com',
-#   password:             'albutler92',
-#   authentication:       'plain',
-#   enable_starttls_auto: true  }
+    config.action_mailer.delivery_method = :smtp
+config.action_mailer.smtp_settings = {
+  address:              'smtp.gmail.com',
+  port:                 587,
+  domain:               'gmail.com',
+  user_name:            'customtechfront@gmail.com',
+  password:             'albutler92',
+  authentication:       'plain',
+  enable_starttls_auto: true  }
 
 
   # Log the query plan for queries taking more than this (works
