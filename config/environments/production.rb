7de955ee15bd5b4e3330afd556712ee4ec9b17e0
Alex Eisenach <hoopmiester@Alexs-MacBook-Pro-3.local>
2013-12-05 18:36:20 -0600

    changed production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,16 +65,17 @@ config.action_mailer.delivery_method = :smtp
 config.action_mailer.perform_deliveries = true
 config.action_mailer.raise_delivery_errors = true
 config.action_mailer.default :charset => "utf-8"
-config.action_mailer.smtp_settings = {
-  address: 'smtp.gmail.com',
-  port: 587,
-  domain:'thawing-shelf-7277.herokuapp.com',
-  authentication:'plain',
-  enable_starttls_auto: true,
-  user_name: 'customtechfront@gmail.com',
-  password: 'theone1390'
-  }
- 
+ActionMailer::Base.smtp_settings = {
+  :address        => 'smtp.sendgrid.net',
+  :port           => '587',
+  :authentication => :plain,
+  :user_name      => ENV['SENDGRID_USERNAME'],
+  :password       => ENV['SENDGRID_PASSWORD'],
+  :domain         => 'heroku.com',
+  :enable_starttls_auto => true
+}
+
+
 
 
 
